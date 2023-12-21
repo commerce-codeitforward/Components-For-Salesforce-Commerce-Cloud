42e3d3ac414a88cc17c961981f19d9d8a4b3cdef
Sandra Golden <sgolden@salesforce.com>
2023-12-21 14:00:41 -0800

    B2B Account Management : add buyer customer and member/user

--- account/accountManagement/lwc/accountManagement/labels.js
+++ account/accountManagement/lwc/accountManagement/labels.js
@@ -1,5 +1,7 @@
+import addUser from '@salesforce/label/c.Account_AccountManagement_addUser';
 import errorDefault from '@salesforce/label/c.Account_AccountManagement_errorDefault';
 
 export {
+    addUser,
     errorDefault
 };
