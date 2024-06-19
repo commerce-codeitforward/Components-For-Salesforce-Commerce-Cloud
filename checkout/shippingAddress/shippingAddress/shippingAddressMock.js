0c267d3054933801d55a76b012134448c7657e8f
Craig Traxler <ctraxler@Craigs-MacBook-Pro-2.local>
2024-06-19 16:01:09 -0700

    revised shipping address with supporting assets

--- checkout/shippingAddress/shippingAddress/shippingAddressMock.js
+++ checkout/shippingAddress/shippingAddress/shippingAddressMock.js
@@ -1,11 +1,9 @@
 const mockedAddressData = {
-    address: {
-      country: "US",
-      stateCode: "NY",
-      city: "New York",
-      postalCode: "11000",
-      street: "123 Broadway"
-    },
+    country: "US",
+    region: "NY",
+    city: "New York",
+    postalCode: "11000",
+    street: "123 Broadway",
     name: "Test Shipping Address",
     id: "2", 
     firstName: "Test",
